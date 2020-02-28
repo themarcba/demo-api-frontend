f6165b5584d9d9ceef3c9252b9bd8bc892eeda76
Marc Backes <mab@mab.lu>
2020-02-28 15:02:03 +0100

    hostname & error

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="hello">
+        <input type="text" v-model="backend" />
         <p v-if="random">random = {{ random }}</p>
         <p v-else>random = null</p>
         <p v-if="secret">secret = {{ secret }}</p>
         <p v-else>secret = null</p>
+        <p v-if="hostname">hostname = {{ hostname }}</p>
+        <p v-else>hostname = null</p>
         <button @click="loadRandom">Load random from API</button>
+        <div class="error" v-if="error">
+            {{ error }}
+        </div>
     </div>
 </template>
 
@@ -13,20 +19,28 @@ export default {
     name: 'HelloWorld',
     data() {
         return {
+            backend: 'http://localhost:8081',
             random: '',
-            secret: ''
+            secret: '',
+            hostname: '',
+            error: ''
         }
     },
     methods: {
         loadRandom() {
-            fetch(`http://localhost:8081/random`)
+            fetch(`${this.backend}/random`)
                 .then(res => res.json())
                 .then(json => {
+                    this.error = ''
                     this.random = json.random
                     this.secret = json.secret
+                    this.hostname = json.hostname
                 })
                 .catch(err => {
-                    console.error(err.message)
+                    this.error = err.message
+                    this.random = ''
+                    this.secret = ''
+                    this.hostname = ''
                 })
         }
     }
@@ -35,7 +49,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+* {
+    box-sizing: border-box;
+}
 p {
     font-family: monospace;
+}
+
+input {
+    font-size: 1.3rem;
+    padding: 0.2rem;
+    width: 100%;
+}
+
+.error {
+    color: tomato;
 }
 </style>
