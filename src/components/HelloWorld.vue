c5bb596ac13da67169211187d175474bfebfb79d
Marc Backes <mab@mab.lu>
2020-03-08 13:16:50 +0100

    changed backend url for tests

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,7 +19,7 @@ export default {
     name: 'HelloWorld',
     data() {
         return {
-            backend: 'http://localhost:8081',
+            backend: 'hhttp://demo-api-backend:8081',
             random: '',
             secret: '',
             hostname: '',
